36a3a4f1c71b56f9334c13d55f718b3e6e419ec5
mkmagaya98 <makomagaya05@gmail.com>
2021-06-29 06:15:30 +0200

    Update st_objectdetect.py

--- st_objectdetect.py
+++ st_objectdetect.py
@@ -35,7 +35,7 @@ from tensorflow.keras.utils import plot_model
 import os
 import glob
 import cv2
-from keras.optimizers import Adam
+from keras.optimizers import adam
 from keras.preprocessing import image
 import requests
 from tensorflow.keras.preprocessing.image import img_to_array, load_img, array_to_img
@@ -58,8 +58,14 @@ model = VGG16(include_top=True, weights='imagenet', input_tensor=None, input_sha
 
 """# Optimization"""
 
-opt = Adam(1e-4)
-model.compile(loss='mse',optimizer=opt)
+# opt = Adam(1e-4)
+opt = adam(lr=0.001, decay=1e-6)
+# model.compile(loss='mse',optimizer=opt)
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=opt,
+    metrics=['accuracy']
+)
 
 # importing pretrained and optimized model 
 # model = load_model('vgg16Model.h5')
